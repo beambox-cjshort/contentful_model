7e35f99a34bf9a937913b8f4e25c7a9398e28bf9
Cal Short <callum@beambox.com>
2020-10-07 12:32:02 +0100

    related method

--- lib/contentful_model/queries.rb
+++ lib/contentful_model/queries.rb
@@ -34,8 +34,8 @@ module ContentfulModel
         query.last
       end
 
-      def related(id, q)
-        query.related(id, q)
+      def related(id, q, n)
+        query.related(id, q, n)
       end
 
       def params(options)
